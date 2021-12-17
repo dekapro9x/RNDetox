097ed3f7954a5135c5365f9df0041e7b29ef508d
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2021-12-17 09:20:59 +0700

    Config react-navigation, root navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
-export default class index extends Component {
-    
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+import {NavigationContainer} from '@react-navigation/native';
+import * as React from 'react';
+import RootNavigations from './navigation/RootNavigations';
+function App() {
+  return (
+    <NavigationContainer>
+      <RootNavigations></RootNavigations>
+    </NavigationContainer>
+  );
 }
+
+export default App;
---
+++ src/screen/AppIntro/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet, View} from 'react-native';
+import {keyNavigation} from '../../navigation/KeyNavigations';
+
+export default function AppIntro({navigation, router}) {
+  const navigateLogin = () => {
+    navigation.navigate(keyNavigation.LOGIN);
+  };
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={navigateLogin}>
+        <Text>APP INTRO</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bnt_mid: {
+    height: 45,
+    width: 250,
+    boderWidth: 1,
+    boderRadius: 10,
+    backgroundColor: 'red',
+  },
+});
